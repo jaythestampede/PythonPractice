67222745497bd65ca31ecd1185aa780e10428685
stamp <jameswboyd32@gmail.com>
2026-01-14 16:58:02 -0500

    Netcat replacement setup

--- CH2/netcat.py
+++ CH2/netcat.py
@@ -6,6 +6,7 @@ import sys
 import textwrap
 import threading
 
+
 def execute(cmd):
     cmd = cmd.strip()
     if not cmd:
@@ -14,7 +15,29 @@ def execute(cmd):
 
     return output.decode()
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description= 'BHP Net Tool'
-    )
+        description='BHP Net Tool',
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        epilog=textwrap.dedent("""\Example:
+        netcat.py -t 192.168.1.108 -p 5555 -l -c # command shell
+        netcat.py -t 192.168.1.108 -p 5555 -l -u=mytest.txt # upload to file
+        netcat.py -t 192.168.1.108 -p 5555 -l -e=\ "cat /etc/passwd\ " # execute command
+        echo 'ABC'| ./netcat.py -t 192.168.1.108 -p 135 # echo text to server port 135
+        netcat.py -t 192.168.1.108 -p 135 -p 5555 # connect to server
+        """))
+    parser.add_argument()
+    parser.add_argument()
+    parser.add_argument()
+    parser.add_argument()
+    parser.add_argument()
+    parser.add_argument()
+    args = parser.parse_args()
+    if args.listen:
+        buffer = ''
+    else:
+        buffer = sys.stdin.read()
+
+    nc = NetCat(args, buffer.encode())
+    nc.run()
